d9338dbd85aa1834c5617398c8c25ce1421f70e7
YeoJoon <joyyoj1@naver.com>
2025-10-28 20:31:14 +0900

    import

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,3 @@
-from .model_config import SMTConfig, TrainingConfig
+from .model_config import SMTConfig, load_config
 
-__all__ = ['SMTConfig', 'TrainingConfig']
+__all__ = ['SMTConfig', 'load_config']
